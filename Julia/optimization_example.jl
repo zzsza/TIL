7adcf0a7de7c65d12392d091856b9fbeaf4e9c95
zzsza <zzsza@naver.com>
2019-07-13 23:51:41 +0900

    [update] julia sample code

---
+++ Julia/optimization_example.jl
@@ -0,0 +1,14 @@
+using JuMP, GLPK
+m = Model(with_optimizer(GLPK.Optimizer))
+
+@variable(m, 0 <= x <= 2 )
+@variable(m, 0 <= y <= 30 )
+
+@objective(m, Max, 5x + 3*y )
+
+@constraint(m, 1x + 5y <= 3.0 )
+
+JuMP.optimize!(m)
+println("Objective value: ", JuMP.objective_value(m))
+println("x = ", JuMP.value(x))
+println("y = ", JuMP.value(y))
